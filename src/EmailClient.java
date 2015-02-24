5eff32f1500a5788b6a68898cfa2723275c60643
jjrocks <jjfunman@gmail.com>
2015-02-24 16:55:36 -0500

    This is why you compile client side first.

--- src/EmailClient.java
+++ src/EmailClient.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Iterator;
-import java.util.stream.Stream;
-
 
 public class EmailClient 
 {
